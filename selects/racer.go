5b6fdf614409d74e6d2d55f87940a868497dfec7
Iman Namvar <iman.namvar@gmail.com>
2022-02-21 10:42:38 +0330

    select tests

---
+++ selects/racer.go
@@ -0,0 +1,45 @@
+package selects
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecoundTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecoundTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+	// aDuration := measureResponseTime(a)
+	// bDuration := measureResponseTime(b)
+	// if aDuration < bDuration {
+	// 	return a
+	// }
+	// return b
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
