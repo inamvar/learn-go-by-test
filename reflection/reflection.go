c548a5c8a0479f885e7fdb896171d1786a83b272
Iman Namvar <iman.namvar@gmail.com>
2022-02-22 11:14:15 +0330

    reflection tests

---
+++ reflection/reflection.go
@@ -0,0 +1,57 @@
+package reflection
+
+import "reflect"
+
+func Walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			Walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		Walk(getField(i).Interface(), fn)
+	}
+
+	// if val.Kind() == reflect.Slice {
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		Walk(val.Index(i).Interface(), fn)
+	// 	}
+	// 	return
+	// }
+	// for i := 0; i < val.NumField(); i++ {
+	// 	field := val.Field(i)
+
+	// 	switch field.Kind() {
+	// 	case reflect.String:
+	// 		fn(field.String())
+	// 	case reflect.Struct:
+	// 		Walk(field.Interface(), fn)
+	// 	}
+	// }
+
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
---
+++ reflection/reflection_test.go
@@ -0,0 +1,117 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with on string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		}, {
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		}, {
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		}, {
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+	// expected := "Chris"
+	// var got []string
+
+	// x := struct {
+	// 	Name string
+	// }{expected}
+
+	// Walk(x, func(input string) {
+	// 	got = append(got, input)
+	// })
+
+	// if len(got) != 1 {
+	// 	t.Errorf("wrong number od function calls , got %d want %d", len(got), 1)
+	// }
+
+	// if got[0] != expected {
+	// 	t.Errorf("got %q, want %q", got[0], expected)
+	// }
+}
